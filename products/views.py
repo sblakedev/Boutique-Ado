a0acd59339554564a1733867043b52c9577b0783
Suzanne Blake <suzanne.blake22@gmail.com>
2022-11-22 15:03:53 +0000

    started product template

--- products/views.py
+++ products/views.py
@@ -6,9 +6,9 @@ from .models import Product
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    
+
     products = Product.objects.all()
-    
+
     context = {
         'products': products,
     }
